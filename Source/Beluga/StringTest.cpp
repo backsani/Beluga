6213747ebb0d888504c37e29aadb8c287c2b93d0
backsani <118903562+backsani@users.noreply.github.com>
2023-07-12 17:52:36 +0900

    Beluga

---
+++ Source/Beluga/StringTest.cpp
@@ -0,0 +1,47 @@
+#include "BelugaMinimal.h"
+
+void StringTest() {
+	
+	// 생성
+		// char		"asdf"
+		// wchar	L"asdf"
+		// tchar	TEXT("asdf")
+
+	// 대입 연산자 = 
+
+	// Printf
+
+	// Format
+
+	// operator []
+
+	// 대소비교
+
+	// TCHAR* 얻기
+	// operator*
+
+	// 길이 구하기
+
+	// Replace
+
+	// Insert
+
+	// clear
+
+	// Split
+
+	// IsEmpty
+
+	// Shrink
+
+	// RemoveAt
+
+	// find 로 인덱스
+
+	// Left
+	// Mid
+	// Right
+
+	// 대문자로 변환, 소문자로 변환
+
+}
