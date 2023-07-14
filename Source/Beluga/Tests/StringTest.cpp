6d01778ffcfdaecc64f0578b7bf57401c4c7b2f7
backsani <118903562+backsani@users.noreply.github.com>
2023-07-14 17:13:14 +0900

    Beluga

--- Source/Beluga/Tests/StringTest.cpp
+++ Source/Beluga/Tests/StringTest.cpp
@@ -6,9 +6,96 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FBelugaStringTest, "Beluga.String", EAutomation
 
 bool FBelugaStringTest::RunTest(const FString& Parameters)
 {
-	TestTrue(TEXT("Always FAIL"), false);
+	{
+		TestTrue(TEXT("Always FAIL"), false);
 
-	//FString str(TEXT("apple"));
+	}
+	//길이 구하기
+	{
+		FString str(TEXT("apple"));
+		int result = str.Len();
+		if(result == 5)
+			TestTrue(TEXT("5"), true);
+		else
+			TestTrue(TEXT("FAIL result : LenFun"), false);
+	}
+	//Left 사용하기
+	{
+		FString str(TEXT("rabbit"));
+		FString result = str.Left(3);
+		TestEqual(TEXT("FAIL result : LeftFun"), result, "rab");
+		
+	}
+	//mid 사용하기
+	{
+		FString str(TEXT("rabbit"));
+		FString result = str.Mid(2,3);
+		TestEqual(TEXT("FAIL result : MidFun"), result, "bbi");
+
+	}
+	//Right 사용하기
+	{
+		FString str(TEXT("rabbit"));
+		FString result = str.Right(3);
+		TestEqual(TEXT("FAIL result : RightFun"), result, "bit");
+
+	}
+	//Replace 사용하기
+	{
+		FString str(TEXT("rabbit"));
+		FString result = str.Replace(TEXT("rabbit"), TEXT("apple"));
+		TestEqual(TEXT("FAIL result : ReplaceFun"), result, "apple");
+
+	}
+	//Insert 사용하기
+	{
+		FString str(TEXT("rabbit "));
+		str.InsertAt(7, TEXT("jump"));
+		TestEqual(TEXT("FAIL result : InsertFun"), str, "rabbit jump");
+	}
+	//Split 사용하기
+	{
+		FString str(TEXT("rabbit jump"));
+		TArray<FString> strArray;
+		strArray.AddDefaulted(2);
+		str.Split(TEXT(" "), &strArray[0], &strArray[1]);
+		TestEqual(TEXT("FAIL result : SplitFun"), strArray[0], "rabbit");
+		TestEqual(TEXT("FAIL result : SplitFun"), strArray[1], "jump");
+	}
+	//IsEmpty 사용하기
+	{
+		FString str(TEXT("rabbit jump"));
+		TestEqual(TEXT("FAIL result : IsEmptyFun"), str.IsEmpty(), false);
+	}
+	//RemoveAt 사용하기 
+	{
+		FString str(TEXT("rabbit"));
+		str.RemoveAt(2,2);
+		TestEqual(TEXT("FAIL result : RemoveAtFun"), str, "rait");
+
+	}
+	//Find 사용하기 
+	{
+		FString str(TEXT("rabbit"));
+		int result = str.Find("b");
+		TestEqual(TEXT("FAIL result : RemoveAtFun"), result, 2);
+
+	}
+	/*
+	FString OriginalString = "Hello World! Welcome to Unreal Engine";
+	TArray<FString> SplitArray;
+	OriginalString.Split(SplitArray, TEXT(" "), ESearchCase::CaseSensitive, ESearchDir::FromStart);
+
+	*/
+	//Shrink 사용하기
+	/*{
+		FString str(TEXT("rabbit"));
+		str.Shrink();
+		TestEqual(TEXT("FAIL result : rabbit"), str, "rabbit");
+	}*/
+
+	//		AddError(FString::Printf(TEXT("Expected '%s' to be true."), What), 1);
+	//		TestEqual(TEXT("Operator []"), strApple[0], L'A');
 
 
 	/*FString strChar("char");
@@ -19,6 +106,7 @@ bool FBelugaStringTest::RunTest(const FString& Parameters)
 	strWChar = "wchar";
 	strTChar = TEXT("wchar");*/
 
+	
 
 	return true;
 }
@@ -57,13 +145,19 @@ bool FBelugaStringTest::RunTest(const FString& Parameters)
 
 	// Shrink
 
-	// RemoveAt
+//	//TArray<int32> ValArr;
+//int32 Temp[] = { 10, 20, 30, 5, 10, 15, 20, 25, 30 };
+//ValArr.Append(Temp, ARRAY_COUNT(Temp));
+//// ValArr == [10,20,30,5,10,15,20,25,30]
+//
+//ValArr.Remove(20);
+// ValArr == [10,30,5,10,15,25,30] 
+
+	//RemoveAt
 
 	// find 로 인덱스
 
-	// Left
-	// Mid
-	// Right
+	
 
 	// 대문자로 변환, 소문자로 변환
 
